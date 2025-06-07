ede6fbd337749b9743b4982e267a6b1679f0674f
Edgar Macias <ef.maciasf@uea.edu.ec>
2025-06-07 13:21:23 -0500

    Suma y resta en C#

--- PAOestructura2025/Program.cs
+++ PAOestructura2025/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    // Suma dos enteros
+    static int Sumar(int a, int b) => a + b;
+
+    // Resta dos decimales
+    static double Restar(double a, double b) => a - b;
+
+    static void Main()
+    {
+        Console.WriteLine("UNIVERSIDAD ESTATAL AMAZÓNICA\n");
+
+        Console.WriteLine("Suma: " + Sumar(4, 6));
+        Console.WriteLine("Resta: " + Restar(10.5, 3.2));
+    }
+}
